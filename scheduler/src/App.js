e5d0fec215885a6b9d4d9ccf8126ad92882aedf2
siseventeen <176776401@qq.com>
2019-09-26 18:11:29 -0500

    Slice 4

--- scheduler/src/App.js
+++ scheduler/src/App.js
@@ -1,6 +1,6 @@
-import React from 'react';
 import 'rbx/index.css';
 import { Button, Container, Title } from 'rbx';
+import React, { useState, useEffect } from 'react';
 
 const schedule = {
   "title":"CS Courses for 2018-2019",
@@ -29,7 +29,7 @@ const schedule = {
 };
 
 const Banner = ({title}) => (
-  <Title>{ title }</Title>
+  <Title>{ title || '[loading...]'}</Title>
   );
 
 const terms = {F:"Fall", W:"Winter", S:"Spring"};
@@ -54,11 +54,27 @@ const CourseList = ({courses}) => (
   </Button.Group>
   );
 
-const App = () => (
-  <Container>
-    <Banner title={ schedule.title } />
-    <CourseList courses={ schedule.courses } />
-  </Container>
+const App = () => {
+  const [schedule, setSchedule] = useState({ title: '', courses: [] });
+  const url = 'https://courses.cs.northwestern.edu/394/data/cs-courses.php';
+  //const proxyUrl = 'https://cors-anywhere.herokuapp.com/';
+
+  useEffect(() => {
+    const fetchSchedule = async () => {
+      const response = await fetch(url); //proxyurl+url
+      if (!response.ok) throw response;
+      const json = await response.json();
+      setSchedule(json);
+    }
+    fetchSchedule();
+  }, [])
+
+  return (
+    <Container>
+      <Banner title={ schedule.title } />
+      <CourseList courses={ schedule.courses } />
+    </Container>
   );
+};
 
 export default App;
